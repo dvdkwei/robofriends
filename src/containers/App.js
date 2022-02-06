d989908ad71704f4f85b014f24d40c9bbc5daef1
dvdkwei <david.kurniadi.weinardy@outlook.com>
2022-02-06 10:47:50 +0700

    robofriends updated with hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,43 +1,31 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import CardList from "../components/CardList";
 // import {robots} from "./robots";
 import SearchBox from "../components/SearchBox";
 import 'tachyons';
 import Scroll from "../components/Scroll";
 
-class App extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            robots: [],
-            searchField: ''
-        }
-    }
+const App = () => {
+    const [robots, setRobots] = useState([]);
+    const [searchField, setSearchField] = useState('');
 
-    componentDidMount() {
+    useEffect( () => {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => response.json())
-            .then(users => this.setState({robots: users}));
-    }
+            .then(data => setRobots(data));
+    }, []); //[robots] means only run when robots changes
 
-    onSearch = (event) => {
-        this.setState({searchField: event.target.value});
-    }
-
-    render() {
-        return (
-            <div align={'center'}>
-                <h1 className={'f1'}>Robofriends</h1>
-                <SearchBox searchChange={this.onSearch}/>
-                <Scroll>
-                    <CardList robots = {
-                        this.state.robots.filter( robot => robot.name.toLowerCase()
-                                    .includes(this.state.searchField.toLowerCase()))
-                    }/>
-                </Scroll>
-            </div>
-        );
-    }
+    return (
+        <div align={'center'}>
+            <h1 className={'f1'}>Robofriends</h1>
+            <SearchBox searchChange={event => setSearchField(event.target.value)}/>
+            <Scroll>
+                <CardList robots={
+                    robots.filter(robot => robot.name.toLowerCase().includes(searchField.toLowerCase()))
+                }/>
+            </Scroll>
+        </div>
+    );
 }
 
 export default App;
